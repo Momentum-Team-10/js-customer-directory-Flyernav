f5e49c2054ec57612b3de078eb308d86213c2bb7
Trey Williams <fly.tech157@gmail.com>
2021-10-13 10:12:50 -0400

    javascript info mostly good, dates fixed, need to fix spacing

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const root = document.getElementById("root");
 const container = document.createElement("div");
 root.appendChild(container);
 container.classList.add("uk-container-large", "uk-flex", "uk-flex-between");
-
+console.log(moment())
 for (let customer of customers) {
     const customerInfo = document.createElement("div");
     customerInfo.id = `${customer.id.value}`;
@@ -15,7 +15,7 @@ for (let customer of customers) {
     );
 
     customerInfo.innerHTML = `
-    <p class="uk-card-title" style="color: red !important; padding: 15px">${customer.title}</p>
+    <p class="uk-card-title" style="color: red !important; padding: 15px">${customer.name.title}</p>
     <img src=${customer.picture.thumbnail} />
     <p><h4>${customer.name.first}</h4></p>
     <p><h4>${customer.name.last}</h4></p>
@@ -25,8 +25,8 @@ for (let customer of customers) {
     <p>${customer.location.state}</p>
     <p>${customer.location.country}</p>
     <p>${customer.email}</p>
-    <p>${customer.dob.date}</p>
-    <p>${customer.registered.date}</p>
+    <p>DOB ${moment(customer.dob.date).format("MMM DD, YYYY")}</p>
+    <p>Customer Since ${moment(customer.registered.date).format("MMM DD, YYYY")}</p>
 `;
 
 
